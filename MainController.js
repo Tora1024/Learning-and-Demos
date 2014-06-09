8988e5f66802d00c9ac5a28884c80d7f5502a003
rquintero <rquinterof@hotmail.com>
2014-06-09 16:49:42 +0000

    modified:   Main.html 	modified:   MainController.js 	new file:   handl 	modified:   music.json 	new file:   startup.sh

--- MainController.js
+++ MainController.js
@@ -1,62 +1,59 @@
 /**
-* Created with AngularLabTest1.
-* User: Tora1024
-* Date: 2014-06-06
-* Time: 04:17 PM
-* To change this template use Tools | Templates.
-*/
-
-app.controller("MainController", function($scope, $http){
-	$scope.inputValue = "";
-
-	
-	
-	$http.get('music.json').then(function(music){
+ * Created with AngularLabTest1.
+ * User: Tora1024
+ * Date: 2014-06-06
+ * Time: 04:17 PM
+ * To change this template use Tools | Templates.
+ */
+app.controller("MainController", function($scope, $http, $parse) {
+    $scope.inputValue = "";
+    
+    $http({method: 'POST', url: 'music.json'}).success(function(music) {
+             
         $scope.selectedPerson = 0;
-		$scope.selectedGenre = null;
-        $scope.people = music.data;
+        $scope.selectedGenre = null;
+        //$scope.people = music.data;
+        
+        console.log(music);
     });
-	
-
-	$scope.peoplee = [
-		{
-			id: 0,
-			name: 'Leon',
-			music: [
-				'Rock',
-				'Metal',
-				'Dubstep',
-				'Electro'
-			]
-		},
-		{
-			id: 1,
-			name: 'Chris',
-			music: [
-				'Indie',
-				'Drumstep',
-				'Dubstep',
-				'Electro'
-			]
-		},
-		{
-			id: 2,
-			name: 'Harry',
-			music: [
-				'Rock',
-				'Metal',
-				'Thrash Metal',
-				'Heavy Metal'
-			]
-		},
-		{
-			id: 3,
-			name: 'Allyce',
-			music: [
-				'Pop',
-				'RnB',
-				'Hip Hop'
-			]
-		}
-	];
+  
+    
+    // example source
+    
+    
+    //console.log(fran);
+    
+    /*
+    
+    var datos = [
+							    {
+							        nombre: 'Matusalén',
+							        edad: 'Desconocida'
+							    },
+							    {
+							        nombre: 'Madonna',
+							        edad: 60
+							    }
+							];
+     
+    $scope.people = [{
+        id: 0,
+        name: 'Leon',
+        music: ['Rock', 'Metal', 'Dubstep', 'Electro']
+    }, {
+        id: 1,
+        name: 'Chris',
+        music: ['Indie', 'Drumstep', 'Dubstep', 'Electro']
+    }, {
+        id: 2,
+        name: 'Harry',
+        music: ['Rock', 'Metal', 'Thrash Metal', 'Heavy Metal']
+    }, {
+        id: 3,
+        name: 'Allyce',
+        music: ['Pop', 'RnB', 'Hip Hop']
+    }];
+    */
+    
+    
 });
