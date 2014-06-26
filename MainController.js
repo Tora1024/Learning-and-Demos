287b43359cb7d0e840e081f99f12b238a48c0930
Tora1024 <heavenhell64@gmail.com>
2014-06-26 21:03:12 +0000

    bla bla

--- MainController.js
+++ MainController.js
@@ -1,19 +1,13 @@
-/**
-* Created with AngularLabTest1.
-* User: Tora1024
-* Date: 2014-06-06
-* Time: 04:17 PM
-* To change this template use Tools | Templates.
-*/
 
 app.controller("MainController", function($scope, $http){
 	
+    console.log("cdfdsfds");
     $scope.selectedPerson = 0;
     $scope.selectedGenre = null;
 	
     $http.get('music.json')
        .then(function(res){
-          console.log("cdfdsfds");
+          
           $scope.people = res.data;                
         });
 });
