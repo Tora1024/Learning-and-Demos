3adc995dae78200cb4162fcffdfb2975f3e2bb15
Hector Coto Rojas <Tora1024@Hectors-MacBook-Pro.local>
2016-09-16 15:12:09 -0600

    Changes that reflects both Transferring Properties and combining components to customise this example. Further analysis could be done here to ensure I am not breaking any good practice, but since it is only practice I'll be digging into that later on.

--- React/Example 2 - Color Palette/Components.js
+++ React/Example 2 - Color Palette/Components.js
@@ -1,3 +1,124 @@
+var options = [
+  {
+      description: 'red',
+      code: '#FF605F'
+  },
+  {
+      description: 'blue',
+      code: '#58B3FF'
+  },
+  {
+      description: 'green',
+      code: '#49DD8E'
+  },
+  {
+      description: 'orange',
+      code: '#FF6663'
+  }
+];
+
+var dropDownOnChange = function(change) {
+    //alert(change.newValue);
+};
+
+var Container = React.createClass({
+  render: function() {
+    return(
+        <Dropdown id='myDropdown' 
+                  options={options} 
+                  value='#FF6663'
+                  labelField='description'
+                  valueField='code'
+                  onChange={dropDownOnChange}/>
+    );
+  }
+});
+
+var Dropdown = React.createClass({
+
+    propTypes: {
+        id: React.PropTypes.string.isRequired,
+        options: React.PropTypes.array.isRequired,
+        value: React.PropTypes.oneOfType(
+            [
+                React.PropTypes.number,
+                React.PropTypes.string
+            ]
+        ),
+        valueField: React.PropTypes.string,
+        labelField: React.PropTypes.string,
+        onChange: React.PropTypes.func
+    },
+
+    getDefaultProps: function() {
+        return {
+            value: null,
+            valueField: 'value',
+            labelField: 'label',
+            onChange: null
+        };
+    },
+
+    getInitialState: function() {
+        var selected = this.getSelectedFromProps(this.props);
+        return {
+            selected: selected
+        }
+    },
+    
+    componentWillReceiveProps: function(nextProps) {
+        var selected = this.getSelectedFromProps(nextProps);
+        this.setState({
+           selected: selected
+        });
+    },
+    
+    getSelectedFromProps(props) {
+        var selected;
+        if (props.value === null && props.options.length !== 0) {
+            selected = props.options[0][props.valueField];
+        } else {
+            selected = props.value;
+        }
+        return selected;
+    },
+
+    render: function() {
+        var self = this;
+        var options = self.props.options.map(function(option) {
+            return (
+                <option key={option[self.props.valueField]} value={option[self.props.valueField]}>
+                    {option[self.props.labelField]}
+                </option>
+            )
+        });
+        return (
+          <div>
+            <Card color = {this.state.selected}/>
+            <select id={this.props.id} 
+                    className='form-control' 
+                    value={this.state.selected} 
+                    onChange={this.handleChange}>
+                {options}
+            </select>
+          </div>
+        )
+    },
+
+    handleChange: function(e) {
+        if (this.props.onChange) {
+            var change = {
+              oldValue: this.state.selected,
+              newValue: e.target.value
+            }
+            this.props.onChange(change);
+        }
+        this.setState({selected: e.target.value});
+    }
+
+});
+
+
 var Square = React.createClass({
   render: function() {
     var squareStyle = {
@@ -38,8 +159,8 @@ var Card = React.createClass({
       };
       return (
         <div style={cardStyle}>
-            <Square color={this.props.color}/>
-          <Label color={this.props.color}/>
+            <Square {...this.props}/>
+            <Label {...this.props}/>
         </div>
       );
     }
@@ -47,7 +168,7 @@ var Card = React.createClass({
 
 ReactDOM.render(
   <div>
-     <Card color="#FF6663"/>
+     <Container/>
   </div>,
   document.querySelector("#container")
 );
