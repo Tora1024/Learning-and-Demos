1e9f291544dab639e87ffbacde2c10a15e7a1d7e
Hector Coto Rojas <heavenhell64@gmail.com>
2016-09-30 10:44:29 -0600

    Making progress towards this app. Also learning a lot more than I expected, since now I am doing things the way it should be, more structured and now fully understanding ES6 code.

--- Example 7 - Youtube Player App/src/index.js
+++ Example 7 - Youtube Player App/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC5NnG_FD3TzJibgtsr3buQzhWt43Xy8TA';
+
 const App = () => {
-	return <div>Hello!</div>;
+	return ( 
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDom.render(
