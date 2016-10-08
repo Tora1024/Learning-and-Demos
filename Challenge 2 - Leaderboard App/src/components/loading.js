668d41d182af7bebb46a2d6d338eeaf6830dfb4a
Hector Coto Rojas <heavenhell64@gmail.com>
2016-10-08 14:25:42 -0600

    Loading page ready

--- Challenge 2 - Leaderboard App/src/components/loading.js
+++ Challenge 2 - Leaderboard App/src/components/loading.js
@@ -4,6 +4,7 @@ const Loading = () => {
 	return (
 		<div className="loader-wrapper">
 		    <div className="loader"></div>
+		    <img className="logo-img" src="../../images/logo.png" />
 		</div>
 	);
 }
